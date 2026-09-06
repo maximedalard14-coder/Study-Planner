ef107a6903e0df6aae657cbf02ed24cbfd9f5793
maximedalard14-coder <maximedalard14@gmail.com>
2026-09-06 23:21:01 +0200

    Migrate project to Maven and add JUnit tests

--- src/test/java/com/studyplanner/StudentTest.java
+++ src/test/java/com/studyplanner/StudentTest.java
@@ -9,14 +9,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StudentTest {
+    Program program = new Program("Data och Systemvetenskap", 180);
+    Student student = new Student(1L, "Maxime", program);
+    Course course = new Course("DA123A", "Java Programming", 7.5);
     @Test
-    void shouldCalculateCompletedCredits(){
-        Program program = new Program("Data och Systemvetenskap", 180);
-        Student student = new Student(1L, "Maxime", program);
-        Course course = new Course("DA123A", "Java Programming", 7.5);
+    void shouldCalculateCompletedCredits() {
+
 
         course.complete();
         student.addCourse(course);
         assertEquals(7.5, student.getCompletedCredits());
+    }
+    @Test
+    void shouldCalculateDegreeProgress(){
+        course.complete();
+        student.addCourse(course);
+        assertEquals( 4.17 , student.getDegreeProgress());
+
+
+    }
+
+    @Test
+    void shouldReturnZeroProgressWhenProgramRequiresZeroCredits(){
+        Program testProgram = new Program("Test Program", 0);
+        Student testStudent = new Student(1L, "Maxime", testProgram);
+
+        assertEquals(0 , testStudent.getDegreeProgress());
     }
 }
